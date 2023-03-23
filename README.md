2f4a61110c13e8bce25d09d1a028cafdccfdf994
samit-maersk <SamitKumar.Patel@maersk.com>
2023-03-23 23:28:37 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 # aws
 - [initial setup](https://registry.terraform.io/providers/hashicorp/aws/latest/docs)
 
+Generate the Access Key and Secrets from IAM than make that avilable on your shell / bash
+
+```sh
+vi ~/.filename
+export AWS_ACCESS_KEY_ID="anaccesskey"
+export AWS_SECRET_ACCESS_KEY="asecretkey"
+Esc , wq!
+
+source ~/.filename
+
+```
--- jumpbox/main.tf
+++ jumpbox/main.tf
@@ -9,24 +9,26 @@ terraform {
 
 # Configure the AWS Provider
 provider "aws" {
-  region = "us-east-1"
-  access_key = "my-access-key" #export AWS_ACCESS_KEY_ID="anaccesskey"
-  secret_key = "my-secret-key" #export AWS_SECRET_ACCESS_KEY="asecretkey"
+  region = "eu-north-1"
+  #access_key = "my-access-key" 
+  #export AWS_ACCESS_KEY_ID="anaccesskey"
+  #secret_key = "my-secret-key" 
+  #export AWS_SECRET_ACCESS_KEY="asecretkey"
 }
 
 #VPC
-resource "aws_vpc" "example" {
-  cidr_block = "10.0.0.0/16"
+resource "aws_vpc" "foo" {
+  cidr_block = "172.16.0.0/16"
   tags = {
     Name = "tf-example"
   }
 }
 
 #SUBNET
-resource "aws_subnet" "my_subnet" {
-  vpc_id            = aws_vpc.my_vpc.id
+resource "aws_subnet" "foo" {
+  vpc_id            = aws_vpc.foo.id
   cidr_block        = "172.16.10.0/24"
-  availability_zone = "us-west-2a"
+  #availability_zone = "eu-north-1a"
 
   tags = {
     Name = "tf-example"
@@ -35,17 +37,18 @@ resource "aws_subnet" "my_subnet" {
 
 #NETWORK INTERFACE
 resource "aws_network_interface" "foo" {
-  subnet_id   = aws_subnet.my_subnet.id
+  subnet_id   = aws_subnet.foo.id
   private_ips = ["172.16.10.100"]
 
   tags = {
-    Name = "primary_network_interface"
+    Name = "tf-example"
   }
 }
 
 #EC2
 resource "aws_instance" "foo" {
-  instance_type = "t2.micro"
+  ami = "ami-0cf72be2f86b04e9b"
+  instance_type = "t3.micro"
 
   network_interface {
     network_interface_id = aws_network_interface.foo.id
@@ -54,5 +57,9 @@ resource "aws_instance" "foo" {
 
   credit_specification {
     cpu_credits = "unlimited"
+  }
+
+  tags = {
+    Name = "tf-example"
   }
 }
